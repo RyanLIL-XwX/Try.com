dd469bd36ffe39a203aeb2b146c6fab6a2967c6f
HanZhen Qin <91490019+RyanLIL-XwX@users.noreply.github.com>
2022-10-15 01:42:57 -0400

    Add files via upload

---
+++ club.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	// $("#hide1").click(function(){
+	// 	$("#context p").hide(3000);
+	// });
+	// $("#show1").click(function(){
+	// 	$("#context p").show(1000);
+	// });
+	$("#s1").click(function(){
+		$(".club li").show(1000);
+	});
+	$("#h1").click(function(){
+		$(".club li").hide(2000);
+	});
+});
